dc543e00104f8419a7ae74f51b350dc397315dbb
abhishek95381 <abhishek95381@gmail.com>
2022-04-18 02:48:57 +0530

    Deisgn is bad, But Profile Creation and Updation Completed

--- ProjectQES/settings.py
+++ ProjectQES/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,16 +132,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Manually Added
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Manually Added
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
--- users/models.py
+++ users/models.py
@@ -5,7 +5,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.CharField(max_length=20)
     categories = models.CharField(max_length=50)
-    age = models.IntegerField()
-    reputation = models.IntegerField(default=None)
+    age = models.IntegerField(null=True)
+    reputation = models.IntegerField(blank=True,null=True)
     phone = models.CharField(max_length=10)
-    profile_pic = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    profile_pic = models.ImageField(default='default.png',upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
